452a41cc47a22699affdbba497e5124b76b1f5dc
Tom Webb <tomwebb1992@gmail.com>
2023-09-27 17:47:08 +0100

    you were only supposed to blow the bloody doors off

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
+  get 'pages/about'
   resources :watchlists
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root to: redirect("/watchlists")
-  # Defines the root path route ("/")
+  root "pages#homepage"  # Defines the root path route ("/")
   # root "articles#index"
 end
